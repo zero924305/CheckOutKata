202a961390c48826bab9a731fc915ba7cfd0ccdd
Altino <altino.chong@univarsolutions.com>
2022-06-25 14:02:21 +0100

    Create Test "Check_ItemPrice_Equal_SkuEnum" passed

--- CheckoutKata Test/UnitTest1.cs
+++ CheckoutKata Test/UnitTest1.cs
@@ -26,5 +26,20 @@ namespace CheckoutKata_Test
             Assert.Equal(nameof(Sku.C), SkuC);
             Assert.Equal(nameof(Sku.D), SkuD);
         }
+
+        [Fact]
+        public void Check_ItemPrice_Equal_SkuEnum()
+        {
+            var SkuA = 50;
+            var SkuB = 30;
+            var SkuC = 20;
+            var SkuD = 15;
+
+            Assert.Equal((int)Sku.A, SkuA);
+            Assert.Equal((int)Sku.B, SkuB);
+            Assert.Equal((int)Sku.C, SkuC);
+            Assert.Equal((int)Sku.D, SkuD);
+
+        }
     }
 }
