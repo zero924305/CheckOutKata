f806cc01b33cb95e34d8c426a47995d1ba1dacc1
Altino <altino.chong@univarsolutions.com>
2022-06-25 14:05:26 +0100

    Add "EnumSku" to production solution

---
+++ CheckoutKata/EnumSku.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class EnumSku
+    {
+        public enum Sku
+        {
+            A = 50,
+            B = 30,
+            C = 20,
+            D = 15
+        }
+    }
+}
