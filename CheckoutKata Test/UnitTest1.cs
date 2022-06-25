d26c0e630aefbba52799bb72c06b745679be7044
Altino <altino.chong@univarsolutions.com>
2022-06-25 13:58:53 +0100

    Create "Check_ItemName_Equal_SkuEnum" passed

--- CheckoutKata Test/UnitTest1.cs
+++ CheckoutKata Test/UnitTest1.cs
@@ -4,10 +4,27 @@ namespace CheckoutKata_Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        public enum Sku
+        { 
+            A = 50,
+            B = 30,
+            C = 20,
+            D = 15
+        }
 
+
+        [Fact]
+        public void Check_ItemName_Equal_SkuEnum()
+        {
+            var SkuA = "A";
+            var SkuB = "B";
+            var SkuC = "C";
+            var SkuD = "D";
+
+            Assert.Equal(nameof(Sku.A), SkuA);
+            Assert.Equal(nameof(Sku.B), SkuB);
+            Assert.Equal(nameof(Sku.C), SkuC);
+            Assert.Equal(nameof(Sku.D), SkuD);
         }
     }
 }
