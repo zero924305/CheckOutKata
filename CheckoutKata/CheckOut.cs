70810592a8cd5748a40f46565ba5eb8633eb4102
Altino <altino.chong@univarsolutions.com>
2022-06-25 14:26:44 +0100

    Add pass function to production

---
+++ CheckoutKata/CheckOut.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class CheckOut
+    {
+
+        public void Scan(string item)
+        {
+            var itemList = ScanItemFunc(item);
+
+            var itemCount = itemList.Count;
+        }
+
+
+        private static List<string> ScanItemFunc(string items)
+        {
+            if (string.IsNullOrEmpty(items))
+                return null;
+
+            var result = new List<string>();
+
+            foreach (var item in items)
+                result.Add(item.ToString());
+
+            return result;
+        }
+
+        private static int CountTotalItem(List<string> itemList)
+        {
+            return itemList.Count;
+        }
+
+    }
+}
